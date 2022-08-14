bcc1f9cc5f1fa558c74962fba2ab398c30a2aa7e
mehmetalierdal19 <mehmetalierdal26@gmail.com>
2022-08-15 02:05:07 +0300

    Blog detaay sayfası yapıldı içerikleri eklendi

--- TravelTrip/TravelTrip/Controllers/BlogController.cs
+++ TravelTrip/TravelTrip/Controllers/BlogController.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TravelTrip.Models.Siniflar;
 
 namespace TravelTrip.Controllers
 {
     public class BlogController : Controller
     {
+        Context c = new Context();
         // GET: Blog
         public ActionResult Index()
         {
-            return View();
+            var bloglar = c.Blogs.ToList();
+            return View(bloglar);
+        }
+        public ActionResult BlogDetay(int id)
+        {
+            var blog = c.Blogs.Where(x => x.ID == id).ToList();
+            return View(blog);
         }
     }
 }
