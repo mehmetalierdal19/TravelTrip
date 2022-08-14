74df61f07d5ff118231beb6c631d3e94ad3205b1
mehmetalierdal19 <mehmetalierdal26@gmail.com>
2022-08-14 06:02:52 +0300

    about ve blog controller ı eklendi sayfa tasarımları yapıldı hakkımzıda sayfasının verileri eklendi

---
+++ TravelTrip/TravelTrip/Controllers/AboutController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTrip.Models.Siniflar;
+
+namespace TravelTrip.Controllers
+{
+    public class AboutController : Controller
+    {
+        Context c = new Context();
+        // GET: About
+        public ActionResult Index()
+        {
+            var degerler = c.Hakkimizdas.ToList();
+            return View(degerler);
+        }
+    }
+}
