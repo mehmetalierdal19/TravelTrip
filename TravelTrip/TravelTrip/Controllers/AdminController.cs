70376ba7e14d2f883ce10bb44c0722f1a31bf550
mehmetalierdal19 <mehmetalierdal26@gmail.com>
2022-08-16 02:43:25 +0300

    Admin controller ı oluşturuldu. tamplate giydirilip türkçeleştirildi. blog listeleme işlemi yapıldı

---
+++ TravelTrip/TravelTrip/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTrip.Models.Siniflar;
+
+namespace TravelTrip.Controllers
+{
+    public class AdminController : Controller
+    {
+        Context c = new Context();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            var degerler = c.Blogs.ToList();
+            return View(degerler);
+        }
+    }
+}
