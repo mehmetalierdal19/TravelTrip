d5fabb722ae10e215e81f738873909e3a08cfa90
mehmetalierdal19 <mehmetalierdal26@gmail.com>
2022-08-15 03:53:49 +0300

    Ana sayfadaki slider resimlerini veritabanından getirme işlemi yapıldı

--- TravelTrip/TravelTrip/Controllers/DefaultController.cs
+++ TravelTrip/TravelTrip/Controllers/DefaultController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TravelTrip.Models.Siniflar;
 
 namespace TravelTrip.Controllers
 {
     public class DefaultController : Controller
     {
+        BlogYorum by = new BlogYorum();
+        Context c = new Context();
         // GET: Default
         public ActionResult Index()
         {
-            return View();
+            by.Deger1 = c.Blogs.ToList();
+            return View(by);
         }
         public ActionResult About()
         {
