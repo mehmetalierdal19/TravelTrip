986066971d40eac1bf65bff7badfb861c3544b68
mehmetalierdal19 <mehmetalierdal26@gmail.com>
2022-08-17 04:22:02 +0300

    login işlemi yapıldı

--- TravelTrip/TravelTrip/Controllers/LoginController.cs
+++ TravelTrip/TravelTrip/Controllers/LoginController.cs
@@ -3,15 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TravelTrip.Models.Siniflar;
+using System.Web.Security;
 
 namespace TravelTrip.Controllers
 {
     public class LoginController : Controller
     {
         // GET: Login
+        Context c = new Context();
         public ActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Admin ad)
+        {
+            var bilgiler = c.Admins.FirstOrDefault(x => x.Kullanici == ad.Kullanici && x.Sifre == ad.Sifre);
+            if(bilgiler != null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgiler.Kullanici, false);
+                Session["Kullanici"] = bilgiler.Kullanici.ToString();
+                return RedirectToAction("Index","Admin");
+            }
+            else
+            {
+                return View();
+            }
         }
     }
 }
